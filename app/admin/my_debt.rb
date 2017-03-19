a1f226eab4ead69da6292dba4660304d5f1b86c5
Raghu Betina <raghu@firstdraft.com>
2017-03-19 04:52:11 +0000

    Whitelist columns in MyDebt for ActiveAdmin

--- app/admin/my_debt.rb
+++ app/admin/my_debt.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register MyDebt do
 
+ permit_params :initiator_id, :other_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
