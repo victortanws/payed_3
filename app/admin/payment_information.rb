dca747bf74964ea61f80f8fad993a36dd32a4953
Raghu Betina <raghu@firstdraft.com>
2017-03-19 04:51:59 +0000

    Whitelist columns in PaymentInformation for ActiveAdmin

--- app/admin/payment_information.rb
+++ app/admin/payment_information.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register PaymentInformation do
 
+ permit_params :cardholder_name, :card_number, :card_type, :cvv, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
